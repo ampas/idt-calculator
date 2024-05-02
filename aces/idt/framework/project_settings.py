218a9b313782cc2abfc8d809d3dd83a775897bfd
Adam Davis <adamdavis@netflix.com>
2024-05-02 15:34:12 +0100

    Add in a framework package and the project settings object which holds specialize idt params which allow not only store values but also allow us to store metadata for each param, such as description, labels and ui type. This allows uis to build the ui dynamically in a decoupled manner

--- aces/idt/framework/project_settings.py
+++ aces/idt/framework/project_settings.py
@@ -6,10 +6,24 @@ from idt.core.structures import BaseSerializable, idt_metadata_property
 
 
 class IDTProjectSettings(BaseSerializable):
+    """ Class which holds the project settings for the IDT maker. The order the properties are created is the order
+        they are serialized and deserialized.
+    """
+
     def __init__(self):
         super().__init__()
+        self._schema_version = IDTProjectSettings.schema_version.metadata.default_value
+        self._aces_transform_id = IDTProjectSettings.aces_transform_id.metadata.default_value
+        self._aces_user_name = IDTProjectSettings.aces_user_name.metadata.default_value
         self._camera_make = IDTProjectSettings.camera_make.metadata.default_value
         self._camera_model = IDTProjectSettings.camera_model.metadata.default_value
+        self._iso = IDTProjectSettings.iso.metadata.default_value
+        self._temperature = IDTProjectSettings.temperature.metadata.default_value
+        self._additional_camera_settings = IDTProjectSettings.additional_camera_settings.metadata.default_value
+        self._lighting_setup_description = IDTProjectSettings.lighting_setup_description.metadata.default_value
+        self._debayering_platform = IDTProjectSettings.debayering_platform.metadata.default_value
+        self._debayering_settings = IDTProjectSettings.debayering_settings.metadata.default_value
+        self._encoding_colourspace = IDTProjectSettings.encoding_colourspace.metadata.default_value
         self._rgb_display_colourspace = IDTProjectSettings.rgb_display_colourspace.metadata.default_value
         self._cat = IDTProjectSettings.cat.metadata.default_value
         self._optimisation_space = IDTProjectSettings.optimisation_space.metadata.default_value
@@ -19,15 +33,19 @@ class IDTProjectSettings(BaseSerializable):
         self._grey_card_reference = IDTProjectSettings.grey_card_reference.metadata.default_value
         self._lut_size = IDTProjectSettings.lut_size.metadata.default_value
         self._lut_smoothing = IDTProjectSettings.lut_smoothing.metadata.default_value
-        self._aces_transform_id = IDTProjectSettings.aces_transform_id.metadata.default_value
-        self._aces_username = IDTProjectSettings.aces_username.metadata.default_value
-        self._iso = IDTProjectSettings.iso.metadata.default_value
-        self._temperature = IDTProjectSettings.temperature.metadata.default_value
-        self._additional_camera_settings = IDTProjectSettings.additional_camera_settings.metadata.default_value
-        self._lighting_setup_description = IDTProjectSettings.lighting_setup_description.metadata.default_value
-        self._debayering_platform = IDTProjectSettings.debayering_platform.metadata.default_value
-        self._debayering_settings = IDTProjectSettings.debayering_settings.metadata.default_value
-        self._encoding_colourspace = IDTProjectSettings.encoding_colourspace.metadata.default_value
+        self._data = IDTProjectSettings.data.metadata.default_value
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.SCHEMA_VERSION)
+    def schema_version(self):
+        return self._schema_version
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ACES_TRANSFORM_ID)
+    def aces_transform_id(self):
+        return self._aces_transform_id
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ACES_USER_NAME)
+    def aces_user_name(self):
+        return self._aces_user_name
 
     @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.CAMERA_MAKE)
     def camera_make(self):
@@ -36,6 +54,34 @@ class IDTProjectSettings(BaseSerializable):
     @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.CAMERA_MODEL)
     def camera_model(self):
         return self._camera_model
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ISO)
+    def iso(self):
+        return self._iso
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.TEMPERATURE)
+    def temperature(self):
+        return self._temperature
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ADDITIONAL_CAMERA_SETTINGS)
+    def additional_camera_settings(self):
+        return self._additional_camera_settings
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.LIGHTING_SETUP_DESCRIPTION)
+    def lighting_setup_description(self):
+        return self._lighting_setup_description
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.DEBAYERING_PLATFORM)
+    def debayering_platform(self):
+        return self._debayering_platform
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.DEBAYERING_SETTINGS)
+    def debayering_settings(self):
+        return self._debayering_settings
+
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ENCODING_COLOUR_SPACE)
+    def encoding_colourspace(self):
+        return self._encoding_colourspace
 
     @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.RGB_DISPLAY_COLOURSPACE)
     def rgb_display_colourspace(self):
@@ -73,38 +119,8 @@ class IDTProjectSettings(BaseSerializable):
     def lut_smoothing(self):
         return self._lut_smoothing
 
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ACES_TRANSFORM_ID)
-    def aces_transform_id(self):
-        return self._aces_transform_id
+    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.DATA)
+    def data(self):
+        return self._data
 
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ACES_USERNAME)
-    def aces_username(self):
-        return self._aces_username
 
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ISO)
-    def iso(self):
-        return self._iso
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.TEMPERATURE)
-    def temperature(self):
-        return self._temperature
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ADDITIONAL_CAMERA_SETTINGS)
-    def additional_camera_settings(self):
-        return self._additional_camera_settings
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.LIGHTING_SETUP_DESCRIPTION)
-    def lighting_setup_description(self):
-        return self._lighting_setup_description
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.DEBAYERING_PLATFORM)
-    def debayering_platform(self):
-        return self._debayering_platform
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.DEBAYERING_SETTINGS)
-    def debayering_settings(self):
-        return self._debayering_settings
-
-    @idt_metadata_property(metadata=ProjectSettingsMetaDataConstants.ENCODING_COLOUR_SPACE)
-    def encoding_colourspace(self):
-        return self._encoding_colourspace
