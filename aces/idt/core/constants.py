6516dc1503956cde51a1f45061cac31171f2a5b7
Adam Davis <adamdavis@netflix.com>
2024-05-11 16:07:50 +0100

    Adding in the constants and structures files Now passing the pre-commit hook styling

---
+++ aces/idt/core/constants.py
@@ -0,0 +1,332 @@
+"""Module which holds constants for the project
+
+"""
+from typing import ClassVar
+
+import colour
+
+from .structures import IDTMetaData
+
+
+class DataFolderStructure:
+    """Constants for the folder names which make up the data structure"""
+
+    DATA: ClassVar[str] = "data"
+    COLOUR_CHECKER: ClassVar[str] = "colour_checker"
+    GREY_CARD: ClassVar[str] = "grey_card"
+
+
+class UITypes:
+    """Constants for the UI categories"""
+
+    INT_FIELD: ClassVar[str] = "IntField"
+    STRING_FIELD: ClassVar[str] = "StringField"
+    OPTIONS_FIELD: ClassVar[str] = "OptionsField"
+    VECTOR3_FIELD: ClassVar[str] = "Vector3Field"
+    FOLDER_STRUCTURE: ClassVar[str] = "FOLDER_STRUCTURE"
+
+
+class UICategories:
+    """Constants for the UI categories"""
+
+    ADVANCED: ClassVar[str] = "Advanced"
+    STANDARD: ClassVar[str] = "Standard"
+    HIDDEN: ClassVar[str] = "HIDDEN"
+
+
+class LUTSize:
+    """Constants for the LUT sizes"""
+
+    LUT_1024: ClassVar[int] = 1024
+    LUT_2048: ClassVar[int] = 2048
+    LUT_4096: ClassVar[int] = 4096
+    LUT_8192: ClassVar[int] = 8192
+    LUT_16384: ClassVar[int] = 16384
+    LUT_32768: ClassVar[int] = 32768
+    LUT_65536: ClassVar[int] = 65536
+    DEFAULT: ClassVar[int] = LUT_1024
+    ALL: ClassVar[tuple[int, ...]] = (
+        LUT_1024,
+        LUT_2048,
+        LUT_4096,
+        LUT_8192,
+        LUT_16384,
+        LUT_32768,
+        LUT_65536,
+    )
+
+
+class RGBDisplayColourspace:
+    """Constants for the RGB display colourspaces"""
+
+    SRGB: ClassVar[str] = "sRGB"
+    DCI_P3: ClassVar[str] = "Display P3"
+    DEFAULT: ClassVar[str] = SRGB
+    ALL: ClassVar[tuple[str, ...]] = (SRGB, DCI_P3)
+
+
+class CAT:
+    """Constants for the CATs"""
+
+    DEFAULT: ClassVar[str] = "CAT02"
+
+    @classmethod
+    @property
+    def ALL(cls):
+        """
+
+        Returns A list of all the CATs
+        -------
+
+        """
+        cats = list(colour.CHROMATIC_ADAPTATION_TRANSFORMS.keys())
+        cats.sort()
+        return cats
+
+
+class OptimizationSpace:
+    """Constants for the optimization spaces"""
+
+    OKLAB: ClassVar[str] = "Oklab"
+    JZAZBZ: ClassVar[str] = "JzAzBz"
+    IPT: ClassVar[str] = "IPT"
+    CIE_LAB: ClassVar[str] = "CIE Lab"
+    DEFAULT: ClassVar[str] = OKLAB
+    ALL: ClassVar[tuple[str, ...]] = (OKLAB, JZAZBZ, IPT, CIE_LAB)
+
+
+class Interpolators:
+    """Constants for the interpolators"""
+
+    CUBIC_SPLINE: ClassVar[str] = "Cubic Spline"
+    LINEAR: ClassVar[str] = "Linear"
+    PCHIP: ClassVar[str] = "PCHIP"
+    SPRAGUE_1880: ClassVar[str] = "Sprague (1880)"
+    DEFAULT: ClassVar[str] = LINEAR
+    ALL: ClassVar[tuple[str, ...]] = (CUBIC_SPLINE, LINEAR, PCHIP, SPRAGUE_1880)
+
+
+class DecodingMethods:
+    """Decoding methods"""
+
+    MEDIAN: ClassVar[str] = "Median"
+    AVERAGE: ClassVar[str] = "Average"
+    PER_CHANNEL: ClassVar[str] = "Per Channel"
+    ACES: ClassVar[str] = "ACES"
+    DEFAULT: ClassVar[str] = MEDIAN
+    ALL: ClassVar[tuple[str, ...]] = (MEDIAN, AVERAGE, PER_CHANNEL, ACES)
+
+
+class ProjectSettingsMetaDataConstants:
+    """Constants for the project settings"""
+
+    SCHEMA_VERSION = IDTMetaData(
+        default_value="0.1.0",
+        description="The project settings schema version",
+        display_name="Schema Version",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.HIDDEN,
+    )
+
+    CAMERA_MAKE = IDTMetaData(
+        default_value="",
+        description="The make of the camera used to capture the images",
+        display_name="Camera Make",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    CAMERA_MODEL = IDTMetaData(
+        default_value="",
+        description="The model of the camera used to capture the images",
+        display_name="Camera Model",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    RGB_DISPLAY_COLOURSPACE = IDTMetaData(
+        default_value=RGBDisplayColourspace.DEFAULT,
+        description="The RGB display colourspace",
+        display_name="RGB Display Colourspace",
+        ui_type=UITypes.OPTIONS_FIELD,
+        options=RGBDisplayColourspace.ALL,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    CAT = IDTMetaData(
+        default_value=CAT.DEFAULT,
+        description="The CAT",
+        display_name="CAT",
+        ui_type=UITypes.OPTIONS_FIELD,
+        options=CAT.ALL,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    OPTIMISATION_SPACE = IDTMetaData(
+        default_value=OptimizationSpace.DEFAULT,
+        description="The optimisation space",
+        display_name="Optimisation Space",
+        ui_type=UITypes.OPTIONS_FIELD,
+        options=OptimizationSpace.ALL,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    ILLUMINANT_INTERPOLATOR = IDTMetaData(
+        default_value=Interpolators.DEFAULT,
+        description="The illuminant interpolator",
+        display_name="Illuminant Interpolator",
+        ui_type=UITypes.STRING_FIELD,
+        options=Interpolators.ALL,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    DECODING_METHOD = IDTMetaData(
+        default_value=DecodingMethods.DEFAULT,
+        description="The decoding method",
+        display_name="Decoding Method",
+        ui_type=UITypes.STRING_FIELD,
+        options=DecodingMethods.ALL,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    EV_RANGE = IDTMetaData(
+        default_value=[-1.0, 0.0, 1.0],
+        description="The EV range",
+        display_name="EV Range",
+        ui_type=UITypes.VECTOR3_FIELD,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    GREY_CARD_REFERENCE = IDTMetaData(
+        default_value=[0.18, 0.18, 0.18],
+        description="The grey card reference",
+        display_name="Grey Card Reference",
+        ui_type=UITypes.VECTOR3_FIELD,
+        ui_category=UICategories.ADVANCED,
+    )
+
+    LUT_SIZE = IDTMetaData(
+        default_value=LUTSize.DEFAULT,
+        description="The LUT size",
+        display_name="LUT Size",
+        ui_type=UITypes.OPTIONS_FIELD,
+        options=LUTSize.ALL,
+        ui_category=UICategories.STANDARD,
+    )
+
+    LUT_SMOOTHING = IDTMetaData(
+        default_value=32,
+        description="The LUT smoothing",
+        display_name="LUT Smoothing",
+        ui_type=UITypes.INT_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    ACES_TRANSFORM_ID = IDTMetaData(
+        default_value="",
+        description="The ACES transform ID",
+        display_name="ACES Transform ID",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    ACES_USER_NAME = IDTMetaData(
+        default_value="",
+        description="The ACES username",
+        display_name="ACES Username",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    ISO = IDTMetaData(
+        default_value=800,
+        description="The ISO",
+        display_name="ISO",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    TEMPERATURE = IDTMetaData(
+        default_value=5600,
+        description="The temperature",
+        display_name="Temperature",
+        ui_type=UITypes.INT_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    ADDITIONAL_CAMERA_SETTINGS = IDTMetaData(
+        default_value="",
+        description="The additional camera settings",
+        display_name="Additional Camera Settings",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    LIGHTING_SETUP_DESCRIPTION = IDTMetaData(
+        default_value="",
+        description="The lighting setup description",
+        display_name="Lighting Setup Description",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    DEBAYERING_PLATFORM = IDTMetaData(
+        default_value="",
+        description="The debayering platform",
+        display_name="Debayering Platform",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    DEBAYERING_SETTINGS = IDTMetaData(
+        default_value="",
+        description="The debayering settings",
+        display_name="Debayering Settings",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    ENCODING_COLOUR_SPACE = IDTMetaData(
+        default_value="",
+        description="The encoding colour space",
+        display_name="Encoding Colour Space",
+        ui_type=UITypes.STRING_FIELD,
+        ui_category=UICategories.STANDARD,
+    )
+
+    DATA = IDTMetaData(
+        default_value={
+            DataFolderStructure.COLOUR_CHECKER: {},
+            DataFolderStructure.GREY_CARD: {},
+        },
+        description="The folder structure for the ",
+        display_name="Schema Version",
+        ui_type=UITypes.FOLDER_STRUCTURE,
+        serialize_group="",
+        ui_category=UICategories.HIDDEN,
+    )
+
+    ALL: ClassVar[tuple[IDTMetaData, ...]] = (
+        SCHEMA_VERSION,
+        CAMERA_MAKE,
+        CAMERA_MODEL,
+        RGB_DISPLAY_COLOURSPACE,
+        CAT,
+        OPTIMISATION_SPACE,
+        ILLUMINANT_INTERPOLATOR,
+        DECODING_METHOD,
+        EV_RANGE,
+        GREY_CARD_REFERENCE,
+        LUT_SIZE,
+        LUT_SMOOTHING,
+        ACES_TRANSFORM_ID,
+        ACES_USER_NAME,
+        ISO,
+        TEMPERATURE,
+        ADDITIONAL_CAMERA_SETTINGS,
+        LIGHTING_SETUP_DESCRIPTION,
+        DEBAYERING_PLATFORM,
+        DEBAYERING_SETTINGS,
+        ENCODING_COLOUR_SPACE,
+        DATA,
+    )
---
+++ aces/idt/core/structures.py
@@ -0,0 +1,193 @@
+"""Module contains a selection of helper classes used throughout the IDT
+
+"""
+import json
+from pathlib import Path
+
+
+class PathEncoder(json.JSONEncoder):
+    """
+    A custom JSONEncoder subclass that knows how to encode Path objects.
+    """
+
+    def default(self, obj):
+        """
+        Convert the object to a JSON serializable object
+
+        Parameters
+        ----------
+        obj : object we want to serialize
+
+        Returns the JSON serializable object
+        -------
+
+        """
+        if isinstance(obj, Path):
+            # Convert the PosixPath to a string
+            return str(obj)
+        # Delegate other types to the default JSONEncoder
+        return super().default(obj)
+
+
+class SerializableConstants:
+    """Constants for the serializable"""
+
+    HEADER = "header"
+    DATA = "data"
+
+
+class IDTMetaData:
+    """Simple class to store metadata information for a property within an IDT project,
+    this data is not used in any calculations or computation and is primarily designed
+    to store data which would be associated with how the property should be displayed
+    in a UI.
+
+    """
+
+    def __init__(
+        self,
+        default_value=None,
+        description="",
+        display_name="",
+        serialize_group=SerializableConstants.HEADER,
+        ui_category="",
+        ui_type="",
+        options=None,
+    ):
+        self.default_value = default_value
+        self.description = description
+        self.display_name = display_name
+        self.serialize_group = serialize_group
+        self.ui_category = ui_category
+        self.ui_type = ui_type
+        self.options = options or []
+
+
+class IDTMetadataProperty:
+    """A property descriptor that stores IDTMetaData and supports
+    both getter and setter functionality.
+    """
+
+    def __init__(self, getter, setter=None, metadata=None):
+        self.getter = getter
+        self.setter = setter
+        self.metadata = metadata
+
+    def __get__(self, instance, owner):
+        """Get the value of the property"""
+        if instance is None:
+            # Return the descriptor itself when accessed from the class
+            return self
+        return self.getter(instance)
+
+    def __set__(self, instance, value):
+        """Set the value of the property"""
+        if self.setter:
+            self.setter(instance, value)
+        else:
+            raise AttributeError("No setter defined for this property")
+
+
+def idt_metadata_property(metadata=None):
+    """
+    Create a property with IDTMetaData, supporting both getter and setter functionality.
+    """
+
+    def wrapper(getter):
+        # Define a setter function to handle setting the value
+        def setter(instance, value):
+            setattr(instance, f"_{getter.__name__}", value)
+
+        # Create a IDTMetadataProperty descriptor with both getter and setter
+        return IDTMetadataProperty(getter, setter, metadata)
+
+    return wrapper
+
+
+class BaseSerializable:
+    """A base class for serializable objects with IDTMetadataProperties that can
+    be converted to and from JSON.
+    """
+
+    @property
+    def properties(self):
+        """Generator for the properties of the object getting the name and property as
+        a tuple
+
+        Yields
+        ------
+        tuple: (str, IDTMetadataProperty)
+
+        """
+        for name, descriptor in self.__class__.__dict__.items():
+            if isinstance(descriptor, IDTMetadataProperty):
+                yield name, descriptor
+
+    def to_json(self):
+        """Convert the object to a JSON string
+
+        Returns
+        -------
+        str: JSON string
+
+        """
+        output = {SerializableConstants.HEADER: {}, SerializableConstants.DATA: {}}
+        for name, prop in self.properties:
+            if prop.metadata.serialize_group:
+                output[prop.metadata.serialize_group][name] = prop.getter(self)
+            else:
+                output[SerializableConstants.DATA] = prop.getter(self)
+        return json.dumps(output, indent=4, cls=PathEncoder)
+
+    @classmethod
+    def from_json(cls, data):
+        """Create a new instance of the class from the given object or json string
+
+        Parameters
+        ----------
+        data: the object or string we want to load json from
+
+        Returns
+        -------
+        BaseSerializable: the loaded object
+
+        """
+        item = cls()
+        if isinstance(data, str):
+            data = json.loads(data)
+
+        for name, prop in item.properties:
+            if prop.metadata.serialize_group == SerializableConstants.HEADER:
+                prop.setter(item, data[SerializableConstants.HEADER][name])
+            else:
+                prop.setter(item, data[SerializableConstants.DATA])
+        return item
+
+    def to_file(self, filepath):
+        """Write the object to a file
+
+        Parameters
+        ----------
+        filepath: str the path to the file
+
+        """
+        with open(filepath, "w") as f:
+            f.write(self.to_json())
+
+    @classmethod
+    def from_file(cls, filepath):
+        """Load the object from a file
+
+        Parameters
+        ----------
+        filepath : str the path to the file
+
+        Returns
+        -------
+        BaseSerializable: the loaded object
+
+        """
+        with open(filepath) as f:
+            data = json.load(f)
+
+        return cls.from_json(data)
