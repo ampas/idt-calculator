5dfe1259857c041a2397a03472ae165a30d7a09b
Adam Davis <adamdavis@netflix.com>
2024-10-09 17:58:54 +0100

    Add the IDTGeneratorColourSpace to the generators

--- aces/idt/generators/__init__.py
+++ aces/idt/generators/__init__.py
@@ -1,4 +1,5 @@
 from .base_generator import IDTBaseGenerator
+from .colour_space_generator import IDTGeneratorColourSpace
 from .prelinearized_idt import IDTGeneratorPreLinearizedCamera
 from .prosumer_camera import IDTGeneratorProsumerCamera
 from .tonemapped_idt import IDTGeneratorToneMappedCamera
@@ -7,6 +8,7 @@ GENERATORS = {
     IDTGeneratorProsumerCamera.GENERATOR_NAME: IDTGeneratorProsumerCamera,
     IDTGeneratorPreLinearizedCamera.GENERATOR_NAME: IDTGeneratorPreLinearizedCamera,
     IDTGeneratorToneMappedCamera.GENERATOR_NAME: IDTGeneratorToneMappedCamera,
+    IDTGeneratorColourSpace.GENERATOR_NAME: IDTGeneratorColourSpace,
 }
 
 __all__ = ["IDTBaseGenerator"]
